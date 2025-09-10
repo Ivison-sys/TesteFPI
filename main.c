e7ee5173196149c907df429cbbd1c9db05b4338a
irs3 <irs3@g2c01.cin.ufpe.br>
2025-09-10 12:58:36 -0300

    Função cadatrar jogador finalizada e função cadastrar pokemom inicializada

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef enum{
     fogo,
     agua,
@@ -25,17 +24,69 @@ typedef struct {
     Pokemon *pokemons;
     int qtdPokemons;
 } Treinador;
+int qtdTreinadores = 0;
+
+// Funcões auxiliares
+int isValidCpf(Treinador* treinadores, char cpf[14]){
+    for(int i = 0; i < qtdTreinadores; i++){
+        if(!strcmp(treinadores[i].cpf, cpf)) return 0;
+    }
+
+    return 1;
+}
 
 Treinador* cadastrarJogador(Treinador* treinadores){
-    printf("Cadastrado");
+    printf("Cadastrado!!\n");
     Treinador treinadorAux;
-    printf("Nome");
+
+    // Dados do treinador
+    printf("Nome: "); scanf(" %s", treinadorAux.nome);
+    printf("CPF: "); scanf(" %s", treinadorAux.cpf);
+    printf("Idade: "); scanf("%d", &treinadorAux.idade);
+    treinadorAux.pokemons = NULL;
+    treinadorAux.qtdPokemons = 0;
+    
+    // Verificando se o cpf é único
+    if(!isValidCpf(treinadores, treinadorAux.cpf)){
+        printf("Esse CPF já consta no sistema!\n");
+        return treinadores;
+    }
+
+    // Adicionado um espaço para o novo treinador
+    Treinador* treinadoresAux = treinadores;
+    treinadores = (Treinador*) realloc(treinadoresAux, (qtdTreinadores + 1) * sizeof(treinadorAux));
+
+    // Boas práticas: Verifcar se a realocação foi bem sucedida.
+    if(treinadores == NULL){
+        printf("Problema de alocacao!\n");
+        free(treinadoresAux);
+        exit(1);
+    }
+
+    treinadores[qtdTreinadores] = treinadorAux;
+    printf("Treinador cadastrado com sucesso!\n\n");
+    qtdTreinadores++;
+    return treinadores;
+    
+}
+
+Treinador* cadastrarPokemon(Treinador* treinadores, char cpf[14]){
+    Pokemon pokemonAux;
+
+    // Dados do pokémon
+    printf("Nome: "); scanf(" %s", pokemonAux.nome);
+    printf("XP (Quantidade de vida): "); scanf("%d", &pokemonAux.xp);
+    printf("Ataque: "); scanf("%d", &pokemonAux.ataque);
+    printf("Defesa: "); scanf("%d", &pokemonAux.defesa);
+    
+    printf("Elemento: "); scanf("");
+
 }
 
 
 int main(){
     Treinador *treinadores = NULL, *treinadoresAux = NULL;
-    int qtdJogadores = 0;
+
     Pokemon *pokemonsAux = NULL;
     int comando;
 
@@ -43,11 +94,11 @@ int main(){
         printf("(1) Cadastrar treinador\n");
         printf("(2) Cadastrar pokémon\n");
         printf("(3) Buscar treinador\n");
-
+        printf("Digite o comando: "); scanf("%d", &comando);
         switch (comando)
         {
         case 1:
-            // cadastrarJogador();
+            treinadores = cadastrarJogador(treinadores);
             break;
         
         default:
